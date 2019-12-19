4d1f9fb1339a3025e24f8bb29a46a9030187f736
Gustavo Novais Rodrigues <gustavonovaisrodrigues@gmail.com>
2019-12-19 03:31:59 -0300

    Adicionado e consertando algumas coisas nas mensagens

--- src/app/publica/cadastrar/cadastrar.page.ts
+++ src/app/publica/cadastrar/cadastrar.page.ts
@@ -2,6 +2,7 @@ import { Usuario } from './../../interfaces/usuario';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cadastrar',
@@ -11,24 +12,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CadastrarPage implements OnInit {
 
   formCadastrar: FormGroup
-
-  constructor(public authService: AuthService, public fb: FormBuilder) { }
+  constructor(public authService: AuthService,
+    public fb: FormBuilder,
+    public loadingController: LoadingController,
+    public toastController: ToastController) { }
 
   ngOnInit() {
-    this.formCadastrar = this.fb.group({
-      nome: ['', [
-        Validators.required,
-        Validators.minLength(3)
-      ]],
-      email: ['', [
-        Validators.required,
-        Validators.email
-      ]],
-      senha: ['', [
-        Validators.required,
-        Validators.minLength(4)
-      ]],
-    })
+    this._formInit()
   }
   get email() {
     return this.formCadastrar.get('email')
@@ -46,15 +36,56 @@ export class CadastrarPage implements OnInit {
       displayName: this.nome.value
     }
   }
-
   async cadastrar() {
+    let load = await this.presentLoadingWithOptions()
+
     if (this.formCadastrar.invalid) return console.log('[Cadastrar Cancelado!!]:')
     let resposta = await this.authService.cadastrarUsuarioSenha(this.novoUsuario, this.senha.value)
 
-    if (!resposta.sucesso) return console.log('[Mensagem do cadastro falha]:', resposta.mensagem)
-    console.log('[Mensagem do cadastro sucesso!!]:', resposta.mensagem)
+    load.dismiss()
+    if (!resposta.sucesso) return this._apresentarMensagem(resposta.mensagem)
+    this._apresentarMensagem(resposta.mensagem, () => { console.log('Redireciona usuario'); })
+  }
+  private _formInit() {
+    this.formCadastrar = this.fb.group({
+      nome: ['gustavo novais', [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      email: ['client6@email.com', [
+        Validators.required,
+        Validators.email
+      ]],
+      senha: ['123456', [
+        Validators.required,
+        Validators.minLength(6)
+      ]],
+    })
+  }
+  private async _apresentarMensagem(mensagem: string, callback?) {
+    const toast = await this.toastController.create({
+      message: mensagem,
+      position: 'top',
+      buttons: [{
+        text: 'Done',
+        handler: () => {
+          if (callback) callback()
+        }
+      }]
+    });
+    toast.present();
   }
 
+  async presentLoadingWithOptions() {
+    const loading = await this.loadingController.create({
+      spinner: 'crescent',
+      message: 'Um momento por favor...',
+      translucent: true,
+      cssClass: 'custom-class custom-loading'
+    });
+    await loading.present();
+    return loading
+  }
 
 
 }
